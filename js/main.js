3baae9f1468e8ccddff77e5775ab24368016c5ee
Inzilia Ziganshina <inzilia53@gmail.com>
2023-05-19 23:17:40 +0300

    add smooth transition on links inside main  page

--- js/main.js
+++ js/main.js
@@ -26,5 +26,11 @@ $(function () {
     $('.menu__btn').on('click', function () {
         $('.menu__list').toggleClass('menu__list--active')
     });
+    $(".menu").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
 
 });

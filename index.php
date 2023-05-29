dbcdb9146fca0de375c978f98bcdf95431e54515
Inzilia Ziganshina <inzilia53@gmail.com>
2023-05-30 01:28:00 +0300

    likes

--- index.php
+++ index.php
@@ -263,6 +263,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="js/slick.min.js"></script>
   <script src="js/main.js"></script>
+  <script src="js/likes.js"></script>
 </body>
 
 </html>

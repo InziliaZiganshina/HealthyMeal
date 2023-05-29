4be4c1fb66c202de163104ad9b5695a661ec951a
Inzilia Ziganshina <inzilia53@gmail.com>
2023-05-29 21:11:42 +0300

    sign in changes

--- server.php
+++ server.php
@@ -50,7 +50,7 @@ if (isset($_POST['reg_user'])) {
   			  VALUES('$username', '$email', '$password')";
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
-  	$_SESSION['success'] = "Теперь вы вошли в систему";
+  /*	$_SESSION['success'] = "Теперь вы вошли в систему";*/
   	header('location: index.php');
   }
 }
